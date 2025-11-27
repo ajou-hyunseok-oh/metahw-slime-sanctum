1c9a88639c4d93d4cf95a15d00871b8a4b4cea21
Hyunseok Oh <developer@triplengames.com>
2025-11-28 08:27:54 +0900

    Add GameManager

---
+++ GameManager.ts
@@ -0,0 +1,10 @@
+import * as hz from 'horizon/core';
+
+class GameManager extends hz.Component<typeof GameManager> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+hz.Component.register(GameManager);
